22657ce303d2dc0a546cc053e9c597e4f2f62759
Alexander F. Rødseth <rodseth@gmail.com>
2023-12-19 20:08:37 +0100

    Minor changes

--- cmd/simple/main.go
+++ cmd/simple/main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"log"
 
-	_ "embed"
-
 	"github.com/xyproto/luapixels"
 )
 
-//go:embed index.lua
-var luaCode string
-
 func main() {
-	if err := luapixels.Run(luaCode); err != nil {
+	if err := luapixels.RunFile("index.lua"); err != nil {
 		log.Fatalln(err)
 	}
 }
